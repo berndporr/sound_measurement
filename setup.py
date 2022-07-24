47df46eb0e9ed1d28223878bcd8e323ab1000713
Bernd Porr <mail@berndporr.me.uk>
2022-07-24 10:57:02 +0100

    Changed a bit of the layout

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='sound_weighting_filters',
-    version='0.9.0',
+    version='0.9.1',
     description="IIR coefficients for A,B,C and ITU_R_468 weighting filters",
     long_description=long_description,
     author='Bernd Porr',
